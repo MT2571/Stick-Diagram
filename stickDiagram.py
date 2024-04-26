27bbaf6909f821c6c72711207e64cef20f7f7b60
MT2571 <21522571@gm.uit.edu.vn>
2024-04-26 20:21:40 +0700

    updated April 26

--- stickDiagram.py
+++ stickDiagram.py
@@ -1,18 +1,20 @@
 # Turtle script example
+import re
 import turtle
-def nMOS1():
-    t.speed(10)
+def draw_A(str):
+    t.speed(5)
     t.width(1)
     t.color('red')
     t.left(90)
     t.forward(50)
+    ls_heads.append(t.pos())
     t.backward(50)
     t.right(90)
     t.forward(50)
     t.write("D", align="right", font=("Arial", 15, "normal"))
     t.left(90)
     t.forward(25)
-    t.write("A", font=("Arial", 15, "normal"))
+    t.write(str[1].upper(), font=("Arial", 15, "normal"))
     t.backward(50)
     t.forward(25)
     t.right(90)
@@ -20,25 +22,27 @@ def nMOS1():
     t.forward(50)
     t.right(90)
     t.forward(50)
+    ls_tails.append(t.pos())
     t.penup()
     t.goto(0, t.ycor() - 50)
     t.left(90)
-    t.pendown
+    t.pendown()
     
     
-def exp_AandB():
-    t.speed(10)
+def draw_AandB(str):
+    t.speed(5)
     t.width(1)
     t.color('red')
     t.left(90)
     t.forward(50)
+    ls_heads.append(t.pos())
     t.backward(50)
     t.right(90)
     t.forward(50)
     t.write("D", align="right", font=("Arial", 15, "normal"))
     t.left(90)
     t.forward(25)
-    t.write("A", font=("Arial", 15, "normal"))
+    t.write(str[1].upper(), font=("Arial", 15, "normal"))
     t.backward(50)
     t.forward(25)
     t.right(90)
@@ -47,7 +51,7 @@ def exp_AandB():
     t.write("D", align="right", font=("Arial", 15, "normal"))
     t.left(90)
     t.forward(25)
-    t.write("C", font=("Arial", 15, "normal"))
+    t.write(str[3].upper(), font=("Arial", 15, "normal"))
     t.backward(50)
     t.forward(25)
     t.right(90)
@@ -55,27 +59,114 @@ def exp_AandB():
     t.forward(50)
     t.right(90)
     t.forward(50)
+    ls_tails.append(t.pos())
     t.penup()
     t.goto(0, t.ycor() - 50)
     t.left(90)
     t.pendown()
     
-    
-t = turtle.Turtle('turtle')
-a = input()
+def draw_AorB(str):
+    t.speed(5)
+    t.width(1)
+    t.color('red')
+    t.left(90)
+    t.forward(50)
+    ls_heads.append(t.pos())
+    t.backward(50)
+    t.right(90)
+    t.forward(50)
+    t.write("D", align="right", font=("Arial", 15, "normal"))
+    t.left(90)
+    t.forward(25)
+    t.write(str[1].upper(), font=("Arial", 15, "normal"))
+    t.backward(50)
+    t.forward(25)
+    t.right(90)
+    t.write("S", font=("Arial", 15, "normal"))
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    ls_tails.append(t.pos())
+    t.backward(50)
+    t.left(90)
+    t.forward(50)
+    t.write("S", align="right", font=("Arial", 15, "normal"))
+    t.left(90)
+    t.forward(25)
+    t.write(str[3].upper(), font=("Arial", 15, "normal"))
+    t.backward(50)
+    t.forward(25)
+    t.right(90)
+    t.write("D", font=("Arial", 15, "normal"))
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.forward(200)
+    t.penup()
+    t.goto(0, t.ycor() - 150)
+    t.left(90)
+    t.left(90)
+    t.pendown()
+
+def connect_2cor(x, y):
+    r = turtle.Turtle()
+    r.penup()
+    r.goto(x)
+    r.pendown()
+    r.goto(y)
+    r.hideturtle()
+
+def format_A_and_B(str):
+    if (len(str) == 5 and str[2] == '*'):
+        return True
+    return False
+def format_A_or_B(str):
+    if (len(str) == 5 and str[2] == '+'):
+        return True
+    return False
+def format_A(str):
+    if (len(str) == 3 and str[1].isalpha()):
+        return True
+    return False
+
+t = turtle.Turtle()
+ls_heads = []
+ls_tails = []
+pattern_AandB = re.compile(r'^(.*.)$')
+y = input("Enter an expression")
+print(y)
 ls = []
-ls1 = ['(', 'a', '*', 'b', ')']
-ls2 = ['+', 'c']
-for i in a:
+for i in y:
     ls.append(i)
-    if (ls == ls1):
-        exp_AandB()
-        ls.clear()   
-    if (ls == ls2):
-        nMOS1()
+    if (i == ')'):
+        j = len(ls) - 1
+        str = ""
+        while (ls[j] != '('):
+            str += ls.pop()
+            j -= 1
+        str += ls.pop()
+        str = "".join(reversed(str))
+        print("str" + str)
+        if (format_A_and_B(str)):
+            draw_AandB(str)
+        if (format_A_or_B(str)):
+            draw_AorB(str)
+        if (format_A(str)):
+            draw_A(str)
+        if (str == "(+)"):
+            connect_2cor(ls_heads[-1], ls_heads[-2])
+            connect_2cor(ls_tails[-1], ls_tails[-2])
+            ls_heads.pop(1)
+            ls_tails.pop(0)
+        if (str == "(*)"):
+            connect_2cor(ls_tails[0], ls_heads[1])
+            ls_heads.pop(1)
+            ls_tails.pop(0)
 print(ls)
-print(ls1)
-if (a == "AB"):
-    exp_AandB()
+print(ls_heads)
+print(ls_tails)
 turtle.done()
+
+
 
